daf0b60e42c39713c2c7120ade3e98fb626c9e62
Justin Roberts <justin.roberts.27@us.af.mil>
2022-11-23 12:15:41 -0700

    remove echo from test all, delete irrelavant notebooks

--- devops-software/test_all.bash
+++ devops-software/test_all.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 TEST_ENV="${TEST_ENV:-}"
-OPAL_ROOT=$(dirname $0)/..
+DEFAULT_OPAL_ROOT=$(dirname $0)/..
+OPAL_ROOT=${OPAL_ROOT:-${DEFAULT_OPAL_ROOT}}
 
 ARGS=$(getopt -o "ahe:o:" \
     --long acceptance,environment:,help,opal-root: \
@@ -10,27 +11,27 @@ ARGS=$(getopt -o "ahe:o:" \
 
 show_help() {
     cat << EOF
-usage: 
-    $0 [-a|--acceptance] 
+usage:
+    $0 [-a|--acceptance]
        [-o|--opal-root OPAL_ROOT]
        [-e|--environment TEST_ENVIRONMENT_NAME]
        [-h|--help]
-    
-    Run all automated tests for the OPAL framework.  Optional tests can be 
-    enabled if a TEST_ENVIRONEMNT_NAME is specified as the test environment. 
+
+    Run all automated tests for the OPAL framework.  Optional tests can be
+    enabled if a TEST_ENVIRONEMNT_NAME is specified as the test environment.
     The only currently supported test environment is "acceptance".
-    
+
     -e|--environment   set testing enviroment to TEST_ENVIRONMENT_NAME
     -a|--acceptance    set testing environment to acceptance. This is equivalent
                        to passing "-e acceptance".
     -o|--opal-root     set directory where opal project is installed
     -h|--help          show this help
-    
+
     TEST_ENVIRONMENT_NAME will default to the environment variable TEST_ENV if
-    it is set.  The command line options will be used first, if specified.  
-    If multiple values on the command line are specified, the last will be 
+    it is set.  The command line options will be used first, if specified.
+    If multiple values on the command line are specified, the last will be
     used.
-    
+
     OPAL_ROOT will default to one directory up from the present working directory.
 EOF
 }
@@ -43,7 +44,7 @@ case "$1" in
         show_help
         exit 0
         ;;
-        
+
     -o | --opal-root )
         OPAL_ROOT=$2
         shift 2
@@ -59,7 +60,7 @@ case "$1" in
         TEST_ENV=acceptance
         shift
         ;;
-        
+
     -- )
         shift
         break
@@ -68,13 +69,6 @@ esac
 done
 
 TEST_ENV=${TEST_ENV,,}
-
-echo "OPAL_ROOT: ${OPAL_ROOT}"
-
-echo "test environment: $TEST_ENV"
-if [[ ${TEST_ENV} == "acceptance" ]] ; then
-    echo "acceptance only scripts"
-fi
 
 echo "-----------------------------------------"
 echo "-------------- tip tests ----------------"
