cb7a3a3d1b79808561c1fc7f773627411d879f29
Justin Roberts <justin.roberts.27@us.af.mil>
2023-01-06 13:48:13 -0700

    pylint updates

--- opal-packages/tests/flow/test_midnight_catalog_generator_flow.py
+++ opal-packages/tests/flow/test_midnight_catalog_generator_flow.py
@@ -1,27 +1,40 @@
 import subprocess
 import metaflow
-from opal.flow.flow_script_utils import *
+from opal.flow.flow_script_utils import delete_run_data
 
 singleuser_python_path = '/opt/conda/envs/singleuser/bin/python'
 torch_python_path = '/opt/conda/envs/torch/bin/python'
-midnight_cat_flow_path ='/home/jovyan/opal/data-engineering-resources/tip_midnight_catalog_generator.py'
+midnight_cat_flow_path = '''/home/jovyan/opal/data-engineering-resources/
+                            tip_midnight_catalog_generator.py'''
 test_artifacts_path = '/home/jovyan/opal/opal-packages/tests/flow/check_artifacts.py'
-      
-    
+
 def test_midnight_flow_torch_create_singleuser_read():
     # Run the flow from torch, read back from single user
-    subprocess.run([torch_python_path, midnight_cat_flow_path, '--no-pylint', 'run', '--tag', 'test'])
-    run = subprocess.run([singleuser_python_path, test_artifacts_path, 'TipMidnightCatalog'], capture_output=True)
-    run.check_returncode()  
-    
+    subprocess.run([torch_python_path, midnight_cat_flow_path,
+                    '--no-pylint',
+                    'run',
+                    '--tag',
+                    'test'])
+    run = subprocess.run([singleuser_python_path,
+                          test_artifacts_path,
+                          'TipMidnightCatalog'], capture_output=True)
+    run.check_returncode()
+
     # clean up
-    opal.flow.flow_script_utils.delete_run_data(list(metaflow.Flow('TipMidnightCatalog').runs('test'))[0])
+    delete_run_data(list(metaflow.Flow('TipMidnightCatalog').runs('test'))[0])
 
 def test_midnight_flow_singleuser_create_torch_read():
     # Run the flow from singleuser, read back from torch
-    subprocess.run([singleuser_python_path, midnight_cat_flow_path, '--no-pylint', 'run', '--tag', 'test'])
-    run = subprocess.run([torch_python_path, test_artifacts_path, 'TipMidnightCatalog'], capture_output=True)
-    run.check_returncode() 
-    
+    subprocess.run([singleuser_python_path,
+                    midnight_cat_flow_path,
+                    '--no-pylint',
+                    'run',
+                    '--tag',
+                    'test'])
+    run = subprocess.run([torch_python_path,
+                          test_artifacts_path,
+                          'TipMidnightCatalog'], capture_output=True)
+    run.check_returncode()
+
     # clean up
-    opal.flow.flow_script_utils.delete_run_data(list(metaflow.Flow('TipMidnightCatalog').runs('test'))[0])
+    delete_run_data(list(metaflow.Flow('TipMidnightCatalog').runs('test'))[0])
