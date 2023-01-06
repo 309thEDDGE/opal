a878d8f8a005e49f1edcf21b2eff276b6d281e35
Justin Roberts <justin.roberts.27@us.af.mil>
2023-01-06 13:12:46 -0700

    pylint fixes

--- opal-packages/tests/flow/test_midnight_catalog_generator_flow.py
+++ opal-packages/tests/flow/test_midnight_catalog_generator_flow.py
@@ -8,19 +8,13 @@ singleuser_python_path = '/opt/conda/envs/singleuser/bin/python'
 torch_python_path = '/opt/conda/envs/torch/bin/python'
 midnight_cat_flow_path ='/home/jovyan/opal/data-engineering-resources/tip_midnight_catalog_generator.py'
 test_artifacts_path = '/home/jovyan/opal/opal-packages/tests/flow/check_artifacts.py'
-
-def check_returncode(run):
-    if run.returncode != 0:
-        print( 'exit status:', run.returncode )
-        print( 'stdout:', run.stdout.decode() )
-        print( 'stderr:', run.stderr.decode() )
-    run.check_returncode()    
+      
     
 def test_midnight_flow_torch_create_singleuser_read():
     # Run the flow from torch, read back from single user
     subprocess.run([torch_python_path, midnight_cat_flow_path, '--no-pylint', 'run', '--tag', 'test'])
     run = subprocess.run([singleuser_python_path, test_artifacts_path, 'TipMidnightCatalog'], capture_output=True)
-    check_returncode(run)
+    run.check_returncode()  
     
     # clean up
     opal.flow.flow_script_utils.delete_run_data(list(metaflow.Flow('TipMidnightCatalog').runs('test'))[0])
@@ -29,7 +23,7 @@ def test_midnight_flow_singleuser_create_torch_read():
     # Run the flow from singleuser, read back from torch
     subprocess.run([singleuser_python_path, midnight_cat_flow_path, '--no-pylint', 'run', '--tag', 'test'])
     run = subprocess.run([torch_python_path, test_artifacts_path, 'TipMidnightCatalog'], capture_output=True)
-    check_returncode(run)
+    run.check_returncode() 
     
     # clean up
     opal.flow.flow_script_utils.delete_run_data(list(metaflow.Flow('TipMidnightCatalog').runs('test'))[0])
