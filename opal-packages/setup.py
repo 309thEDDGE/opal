3a39bc97472273bb36b9a0c3e155504f7cac330b
Nathan Christiansen <nathan.christiansen.1@us.af.mil>
2023-03-14 19:15:04 +0000

    create index from the command line

--- opal-packages/setup.py
+++ opal-packages/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="opal-packages",
     version="0.1",
-    packages=["opal.flow", "opal.publish", "opal.query"],
+    packages=["opal.flow", "opal.publish", "opal.query", "opal.catalog"],
     install_requires=["metaflow", "numpy", "pandas", "s3fs", "requests"],
     package_data={"opal.flow_utils": ["resources/flow_script_upload.py"]},
 )
