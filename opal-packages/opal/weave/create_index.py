9d658b3a619b000c3617702814714b9678c37509
Nathan Christiansen <nathan.christiansen.1@us.af.mil>
2023-03-21 20:52:36 +0000

    pylint create_index.py

--- opal-packages/opal/weave/create_index.py
+++ opal-packages/opal/weave/create_index.py
@@ -1,30 +1,42 @@
 '''
 USAGE:
 python create_index.py <root_dir> <schema_path>
-root_dir: the root directory of s3 you wish to build your index off of
-<<<<<<< HEAD
-schema_path: the path to a local json file that specifies basket keys
-=======
-schema_path: the path to a local json file that specifies datum keys
->>>>>>> 01b3a38 (save changes)
+    root_dir: the root directory of s3 you wish to build your index off of
+    schema_path: the path to a local json file that specifies basket keys
 '''
 
 import json
+import argparse
 import pandas as pd
 import opal.flow
-import argparse
+
 
 #validate basket keys and value data types on read in
 
 def validate_basket_json(basket_dict, schema, basket_address):
+    """
+    validate the basket_manifest.json has the correct structure
+    
+    Parameters
+        basket_dict: dictionary read in from basket_manifest.json in minio
+        schema: loaded from schema_path passed to create_index_from_s3
+        basket_address: basket in question. Passed here to create better error message
+    """
     if list(basket_dict.keys()) != schema:
         raise ValueError(f'basket found at {basket_address} has invalid schema')
-        
+
     ########## validate types for each key
-    
+
     return basket_dict
 
 def create_index_from_s3(root_dir, schema_path):
+    """
+    Recursively parse an s3 bucket and create an index using basket_manifest.json found therein
+    
+    Parameters:
+        root_dir: path to s3 bucket
+        schema_path: path to json file that specifies structure of basket_manifest.json
+    """
     opal_s3fs = opal.flow.minio_s3fs()
 
     basket_jsons = [x for x in opal_s3fs.find(root_dir) if x.endswith('basket_manifest.json')]
@@ -36,7 +48,7 @@ def create_index_from_s3(root_dir, schema_path):
         index_dict[key] = []
     index_dict['address'] = []
     index_dict['storage_type'] = []
-    
+
     for basket_json in basket_jsons:
         basket_address = f's3://{basket_json}'
         with opal_s3fs.open(basket_address, 'rb') as file:
@@ -47,14 +59,15 @@ def create_index_from_s3(root_dir, schema_path):
             #index_dict['uuid'] = index_dict['uuid'].astype(int64)
             index_dict['address'].append(basket_address)
             index_dict['storage_type'].append('s3')
-            
-    index = pd.DataFrame(index_dict) 
+
+    index = pd.DataFrame(index_dict)
     index['uuid'] = index['uuid'].astype(str)
     return index
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(
-        description="Save a local index of an s3 bucket built off of basket_details.json found within said bucket."
+        description="""Save a local index of an s3 bucket built off of 
+                        basket_details.json found within said bucket."""
     )
     argparser.add_argument(
         "root_dir",
@@ -68,7 +81,7 @@ if __name__ == "__main__":
         type=str,
         help="the path to a local json file that specifies basket keys",
     )
-    
+
     args = argparser.parse_args()
-    
+
     create_index_from_s3(args.root_dir, args.schema_path).to_parquet('index.parquet')
