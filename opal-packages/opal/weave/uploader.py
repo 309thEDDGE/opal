e845459ab39b2299cc1bafc538585268f4837104
JR-USAF <justin.roberts.27@us.af.mil>
2023-03-30 20:34:25 +0000

    update doc string

--- opal-packages/opal/weave/uploader.py
+++ opal-packages/opal/weave/uploader.py
@@ -108,29 +108,49 @@ def upload_basket(upload_items,
                  label = '',
                  **kwargs):
     """
-    Upload a directory of data to MinIO. 
+    Upload files and directories to MinIO. 
 
-    This function takes in a local directory path along with
-    taging information and creates a metadata.json file and
-    a basket_details.json file. These two files together with 
-    the data from local_dir_path are uploaded to the 
-    upload_directory path within MinIO as a basket of data. 
+    This function takes in a list of items to upload along with
+    taging information and uploads the data together with three
+    json files: basket_manifest.json, basket_metadata.json, and
+    supplement.json. The contents of the three files are specified 
+    below. These three files together with the data specified by
+    upload_items are uploaded to the upload_directory path within 
+    MinIO as a basket of data. 
     
-    basket_details.json contains:
+    basket_manifest.json contains:
         1) unique_id
         2) list of parent ids
         3) basket type
         4) label
         5) upload date
         
-    metadata.json contains:
+    basket_metadata.json contains:
         1) dictionary passed in through the metadata parameter
+        
+    basket_supplement.json contains:
+        1) the upload_items object that was passed as an input parameter
+        2) integrity_data for every file uploaded 
+            a) file checksum
+            b) upload date
+            c) file size (bytes)
+            d) source path (original file location) 
+            e) stub (true/false)
+            f) upload path (path to where the file is uploaded in MinIO)
 
     Parameters
     ----------
-    local_dir_path : str
-        Path to local directory containing data to be 
-        uploaded to MinIO.
+    upload_items : [dict]
+        List of python dictionaries with the following schema:
+        {
+            'path': path to the file or folder being uploaded (string),
+            'stub': true/false (bool)
+        }
+        'path' can be a file or folder to be uploaded. Every filename
+        and folder name must be unique. If 'stub' is set to True, integrity data
+        will be included without uploading the actual file or folder. Stubs are 
+        useful when original file source information is desired without uploading
+        the data itself. This is especially useful when dealing with large files.
     upload_directory: str
         MinIO path where basket is to be uploaded.
     unique_id: int
@@ -166,7 +186,7 @@ def upload_basket(upload_items,
     unallowed_filenames = ['basket_manifest.json', 'basket_metadata.json', 'basket_supplement.json']
     for upload_item in upload_items:
         validate_upload_item(upload_item)
-        local_path_basename = os.path.basename(upload_item['path'])
+        local_path_basename = os.path.basename(Path(upload_item['path']))
         if local_path_basename in unallowed_filenames:
             raise ValueError(f"'{local_path_basename}' filename not allowed")
         # Check for duplicate file/folder names
