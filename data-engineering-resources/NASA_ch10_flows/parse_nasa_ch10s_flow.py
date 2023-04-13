c949a95129e20b70b8c54ef2a33ebfe9cc52f671
Nathan Christiansen <nathan.christiansen.1@us.af.mil>
2023-04-13 21:15:27 +0000

    ready for PR

--- data-engineering-resources/NASA_ch10_flows/parse_nasa_ch10s_flow.py
+++ data-engineering-resources/NASA_ch10_flows/parse_nasa_ch10s_flow.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import s3fs
 import tempfile
 import subprocess
@@ -39,13 +40,7 @@ class NASAch10ParseFlow(opal.flow.OpalFlowSpec):
                     # load the yaml and save it in metaflow
                     with open(meta_file) as f:
                         metadata = yaml.safe_load(f)
-                        print(f"Found metadata for {metadata['type']}")
                         tip_metadata[metadata["type"]] = metadata
-
-                        # copy some chapter 10 metadata for convenience
-                        # resources = tip_metadata["provenance"]["resource"]
-                        # ch10_resource = [r for r in resources if r["type"] == "CH10"]
-                        # self.ch10_metadata = ch10_resource[0] if ch10_resource else {}
 
         # we should find at least one
         if not tip_metadata:
@@ -64,23 +59,21 @@ class NASAch10ParseFlow(opal.flow.OpalFlowSpec):
 
     @step
     def parse_ch10s(self):
-        '''
-        '''
+        '''Download ch10, parse it, upload output of tip_parse'''
         opal_s3fs = s3fs.S3FileSystem(client_kwargs = {'endpoint_url': os.environ['S3_ENDPOINT']})
         if not opal_s3fs.exists(self.bucket_name):
             raise FileNotFoundError(f"Specified Bucket Not Found: {self.bucket_name}")
         
+        #create an index, get the address column, and get the first n
         index = create_index_from_s3(self.bucket_name, 'schema.json')
         ch10_index = index[index['basket_type'] == 'ch10']
         self.ch10_baskets = ch10_index['address']
-
         if self.n is not None and self.n < len(self.ch10_baskets):
             self.ch10_baskets = self.ch10_baskets[:self.n]
 
-        for basket in self.ch10_baskets:
+        num_ch10s = len(self.ch10_baskets)
+        for i, basket in enumerate(self.ch10_baskets):
             basket_contents = opal_s3fs.ls(basket)
-            ##################### get uuid from basket_manifest
-            parent_uuid = os.path.basename(basket)
             
             #check that there is one ch10 and get the path to it
             rch10_path = [x for x in basket_contents if x.endswith('ch10')]
@@ -88,11 +81,12 @@ class NASAch10ParseFlow(opal.flow.OpalFlowSpec):
                 print(f'there are {len(rch10_path)} ch10s in basket {basket}. skipping.')
                 continue
             rch10_path = rch10_path[0]
+            
+            print(f'{i+1}/{num_ch10s}: {rch10_path}')
 
             ch10_filename = os.path.basename(rch10_path)
             ch10_path = os.path.join(self.local_dir_path, ch10_filename)
             opal_s3fs.get(rch10_path, ch10_path)
-            print(ch10_path)
             
             #run tip parse
             subprocess.run(
@@ -118,6 +112,12 @@ class NASAch10ParseFlow(opal.flow.OpalFlowSpec):
                 ch10_name = basket_metadata['ch10name']
             
             tip_metadata['ch10name'] = ch10_name
+            
+            #get parent_uuid from basket_manifest
+            parent_uuids = []
+            with opal_s3fs.open(os.path.join(basket, 'basket_manifest.json'), 'rb') as file:
+                basket_manifest = json.load(file)
+                parent_uuids.append(basket_manifest['uuid'])
 
             #build upload_dicts
             upload_dicts = []
@@ -128,13 +128,13 @@ class NASAch10ParseFlow(opal.flow.OpalFlowSpec):
                                         self.bucket_name,
                                        'ch10_parsed',
                                         label = ch10_name,
-                                        parent_ids = [parent_uuid],
+                                        parent_ids = parent_uuids,
                                         metadata = tip_metadata)
 
             # clean out temp_dir
             for f in os.scandir(self.local_dir_path):
-                if f.is_dir:
-                    os.rmdir(f.path)
+                if f.is_dir():
+                    shutil.rmtree(f.path)
                 else:
                     os.remove(f.path)
 
