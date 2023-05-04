6983ce2f1c02810f373c067f6f94f13a72f4e20f
JR-USAF <justin.roberts.27@us.af.mil>
2023-05-04 22:20:20 +0000

    note added

--- opal-packages/opal/weave/config.py
+++ opal-packages/opal/weave/config.py
@@ -1,6 +1,8 @@
 '''
 config.py provides configuration settings used by weave.
 '''
+# As schema change, a parameter can be passed to index_schema
+# to determine which schema to return.
 def index_schema():
     '''
     Return the keys expected from the manifest.json file.
