8aa2daf03c381c678d192c6e6b1d40268f28872c
CW-USAF <carson.wilkins@us.af.mil>
2023-08-03 21:27:32 +0000

    Deleting run_metaflow.ipynb

--- test-notebooks/test_run_metaflow.py
+++ test-notebooks/test_run_metaflow.py
@@ -29,10 +29,34 @@ def test_files_in_data_files_actually_exist(opal_flow_and_run):
 
 def test_tagged_as_no_data(opal_flow_and_run):
     up_run = opal_flow_and_run[2]
+    
     assert 'no_data' not in up_run.tags
 
 
 def test_data_files_deleted(opal_flow_and_run):
     s3 = opal_flow_and_run[0]
     up_run = opal_flow_and_run[2]
+    
     assert s3.ls(up_run.data.data_files['file'])
+    
+
+def test_frozenset_up_run_tags(opal_flow_and_run):
+    up_run = opal_flow_and_run[2]
+    
+    assert type(up_run.tags) == frozenset
+    
+    
+def test_size_of_ex_run_data_files(opal_flow_and_run):
+    import pandas as pd
+    
+    s3 = opal_flow_and_run[0]
+    ex_run = opal_flow_and_run[1]
+    
+    ex_run_df = pd.read_parquet(ex_run.data.data_files['parquet_table'], 
+                                filesystem=s3)
+    ex_run_df_shape = ex_run_df.shape
+    
+    assert ex_run_df_shape == (64, 1)
+    
+    
+                     
